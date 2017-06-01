1fc4f106ed8507711d6e05417cff73e2758d0e13
Ehbraheem <ehbraheem@gmail.com>
2017-06-01 10:53:38 -0700

    Add the Mongoid method for custom Point type

--- models/point.rb
+++ models/point.rb
@@ -2,8 +2,40 @@ class Point
 
 	attr_reader :lng, :lat
 
-	def initialize(**hash)
-		@lat, @lng = hash
+	def initialize(arr)
+		@lat, @lng = arr
+	end
+
+	def to_a
+		[@lat, @lng]
+	end
+
+	def mongoize
+		{type: self.class.name, coordinates: self.to_a}
+	end
+
+	def self.mongoize object
+		case object
+		when Point then object.mongoize
+		when Hash then
+			if object[:type] # GeoJSON format
+				Point.new(object[:coordinates]).mongoize
+			else
+				Point.new(object.values).mongoize
+			end
+		else object
+		end
+	end
+
+	def self.demongoize object
+		Point.new(object[:coordinates])
+	end
+
+	def self.evolve object
+		case object 
+		when Point then object.mongoize
+		else object
+		end
 	end
 
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -4,7 +4,7 @@ require_relative File.absolute_path "./models/point.rb"
 
 RSpec.describe "Points" do
 	
-	let(:point) { Point.new }
+	let(:point) { Point.new([23, 45]) }
 
 	describe "valid object" do
 
@@ -28,6 +28,37 @@ RSpec.describe "Points" do
 		it "does not respond to setter methods" do
 			expect(point).to_not respond_to :lat=
 			expect(point).to_not respond_to :lng=
+		end
+	end
+
+	describe "to MongoDB transformation" do
+		
+		it "can convert to MongoDB representation" do
+			expect(point).to respond_to :mongoize
+			expect(point.mongoize).to have_key :type
+			expect(point.mongoize[:type]).to eq point.class.name
+			# expect(point.mongoize).to 
+		end
+
+		it "has important .methods for transformation" do
+			expect(Point).to respond_to :mongoize
+			expect(Point).to respond_to :demongoize
+			expect(Point).to respond_to :evolve
+		end
+
+		it "can tranform any form to MongoDB form" do
+			point_object = point
+			hash_object = {:type => point.to_s, :coordinates => point.to_a}
+			nil_object = nil
+
+			expect(Point.mongoize(hash_object)).to eq point.mongoize
+			expect(Point.mongoize(point)).to eq point.mongoize
+			expect(Point.mongoize(nil_object)).to eq nil
+		end
+
+		it "can tranform MongoDB for form to object form" do
+			expect(Point.demongoize(point.mongoize).lat).to eq point.lat
+			expect(Point.demongoize(point.mongoize).lng).to eq point.lng
 		end
 	end
 
