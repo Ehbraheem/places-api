77a2da0d3ce2328f808dde6a176bc3c69f3b4861
Ehbraheem <ehbraheem@gmail.com>
2017-06-06 22:24:29 -0700

    Create migration for Category

--- db/migrate/20170607051746_create_category.rb
+++ db/migrate/20170607051746_create_category.rb
@@ -1,7 +1,7 @@
 class CreateCategory < ActiveRecord::Migration[5.1]
   def up
   	create_table :category do |t|
-  		t.title String, null: false
+  		t.string :title, null: false
 
   		t.references :location, index: true, foreign_key: true, null: false
   	end
