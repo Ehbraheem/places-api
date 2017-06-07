32a1ae636e30419e03866bcea6f45b9dec8bc559
Ehbraheem <ehbraheem@gmail.com>
2017-06-06 17:21:54 -0700

    Fix rake migration NoDataBaseError

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,13 @@
 
 Sinatra::Base.configure :production, :development, :test do 
 
+	require "byebug"
+
 	# Bootstrap MongoDB connection
 	Mongoid.load! "./config/mongoid.yml"  # we are running this method from top-level
 
-
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/places_api')
+	byebug
+	db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/places_api_#{ENV["RACK_ENV"]}")
 
 	ActiveRecord::Base.establish_connection(
 		:adapter   => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
