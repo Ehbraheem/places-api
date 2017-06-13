ef3881584656be62202a3ec5ef490f6abd2813e5
Ehbraheem <ehbraheem@gmail.com>
2017-06-12 19:17:36 -0700

    Fix typo in Category model

--- models/category.rb
+++ models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveRecord::Base
 
-	has_many :landmaks, inverse_of: :category
+	has_many :landmarks, inverse_of: :category
 
-	has_many :locations, through: :landmaks
+	has_many :locations, through: :landmarks
 
 	has_many_documents :places #, inverse_of: :category
 
