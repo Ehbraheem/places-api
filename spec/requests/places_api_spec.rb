698ffd97545c0aaccaab51b3c95e9ec2e6f58e2a
Ehbraheem <ehbraheem@gmail.com>
2017-06-15 20:25:28 -0700

    Add specs for other possible routes

--- spec/requests/places_api_spec.rb
+++ spec/requests/places_api_spec.rb
@@ -1,55 +1,175 @@
 require File.expand_path "../../spec_helper.rb", __FILE__
 
-RSpec.describe "Places API", type: :requests do
+include ApiHelper
+
+RSpec.describe "Places API", type: :request do
 	
 	include_context "db_cleanup_each" 
 
 	describe "Root page" do
+
+		before(:each) do
+			jget base_path
+		end 
 		
 		describe "Empty database" do
 
-			context "Return type" do
-				
-				it "returns HTTP status OK"
-			
-				it "returns empty JSON document" 
+			it_should_behave_like "Check response"
 
-			end
+			it_should_behave_like "Return type"
 
-			context "Valid response" do
-				
-				it "HTTP status is 2xx"
-
-				it "returns valid document from DB"
-
-				it "returns all available document"
-
-			end
+			it_should_behave_like "Valid response"
 
 		end
 
 		describe "Non-empty database" do
 
-			context "Return type" do
-				
-				it "returns HTTP status OK"
+			it_should_behave_like "Check response"
 
-				it "returns all documents"
+			it_should_behave_like "Return type"
 
-			end
-
-			context "Valid response" do
-				
-				it "HTTP status is 2xx"
-
-				it "returns valid document from DB"
-
-				it "returns all available document"
-
-			end
+			it_should_behave_like "Valid response"
 
 		end
 
-		context ""
+	end
+
+	describe "Locations page" do
+
+		before(:each) do
+			jget locations_path
+		end   
+		
+		describe "Empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+		end
+
+		describe "Non-empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+			it_should_behave_like "Return all data currently availabe", :location 
+
+			it_should_behave_like "return accurate number of records", :location
+
+			it_should_behave_like "verify JSON docuemnt returned", [:location, :name]
+
+			it_should_behave_like "verify data", :location
+
+		end
+
+	end
+
+	describe "Category page" do
+
+		before(:each) do
+			jget categories_path
+		end 
+		
+		describe "Empty database" do
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+		end
+
+		describe "Non-empty database" do
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+			it_should_behave_like "Return all data currently availabe", :category 
+
+			it_should_behave_like "return accurate number of records", :category
+
+			it_should_behave_like "verify JSON docuemnt returned", [:category, :title]
+
+			it_should_behave_like "verify data", :category
+
+		end
+
+	end
+
+	describe "Places page" do
+
+		before(:each) do
+			jget places_path
+			it_should_behave_like
+		end 
+		
+		describe "Empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+		end
+
+		describe "Non-empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+			it_should_behave_like "Return all data currently availabe", :place 
+
+			it_should_behave_like "return accurate number of records", :place
+
+			it_should_behave_like "verify JSON docuemnt returned", [:place, :name]
+
+			it_should_behave_like "verify data", :place
+
+		end
+
+	end
+
+	describe "Place page" do
+
+		before(:each) { jget places_path(23)  }
+		
+		describe "Empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+		end
+
+		describe "Non-empty database" do
+
+			it_should_behave_like "Check response"
+
+			it_should_behave_like "Return type"
+
+			it_should_behave_like "Valid response"
+
+			it_should_behave_like "Return all data currently availabe", :place 
+
+			it_should_behave_like "return accurate number of records", :place
+
+			it_should_behave_like "verify JSON docuemnt returned", [:place, :name]
+
+			it_should_behave_like "verify data", :place
+
+		end
+
 	end
 end
