0949016b5e9c37e4aa55ce8b02cba27aa2903912
Ehbraheem <ehbraheem@gmail.com>
2017-06-16 17:36:03 -0700

    Add Scope to Location and before_save callback

--- models/location.rb
+++ models/location.rb
@@ -6,5 +6,13 @@ class Location < ActiveRecord::Base
 
 	validates_presence_of :name, unique: true
 
-	
+	scope :with_name, -> (name) { where(:name => name).first }
+
+	# scope :for_category, -> { with_name.categories }
+
+	before_save :downcase_name
+
+	def downcase_name
+		self.name = name.gsub(/\s/, "+").downcase
+	end
 end
