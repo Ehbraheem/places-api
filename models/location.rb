b448a0c9687e59b0d05724c4e113698159ed7a2d
Ehbraheem <ehbraheem@gmail.com>
2017-06-19 10:11:07 -0700

    REmove scope and add Class method instead

--- models/location.rb
+++ models/location.rb
@@ -4,13 +4,18 @@ class Location < ActiveRecord::Base
 
 	has_many :categories, through: :landmarks
 
-	validates_presence_of :name, unique: true
+	validates_presence_of :name
+	validates_uniqueness_of :name
 
-	scope :with_name, -> (name) { where(:name => name).first }
+	# scope :with_name, -> (name) { find_by(:name => name) }
 
 	# scope :for_category, -> { with_name.categories }
 
 	before_save :downcase_name
+
+	def self.with_name name
+		find_by(:name => name)
+	end
 
 	def downcase_name
 		self.name = name.gsub(/\s/, "+").downcase
