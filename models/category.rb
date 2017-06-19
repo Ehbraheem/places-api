f9d1d685622963397e83114ac5dc3b85e2ff2b9f
Ehbraheem <ehbraheem@gmail.com>
2017-06-19 10:11:35 -0700

    REmove scope and add Class method instead

--- models/category.rb
+++ models/category.rb
@@ -1,13 +1,18 @@
 class Category < ActiveRecord::Base
 
-	has_many :landmarks, inverse_of: :category
+	has_many :landmarks, inverse_of: :category, dependent: :destroy
 
 	has_many :locations, through: :landmarks
 
 	has_many_documents :places #, inverse_of: :category
 
 	validates_presence_of :title, unique: true
+	validates_uniqueness_of :title
 
-	scope :with_title, ->(title) { where(:title=>title).first }
+	scope :with_title, -> (title) { find_by(:title => title) }
+
+	def self.with_title title
+		find_by(:title => title)
+	end
 
 end
